0d8e77168e75ef981c1d55f1baf3903829b9cecd
Rupesh Tandan <rupeshkumart03@gmail.com>
2026-08-20 18:30:11 +0530

    Add files via upload

---
+++ emotion.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+img_size = 48
+batch_size = 64
+
+train_path = "images/train"
+test_path = "images/validation"
+
+
+datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = datagen.flow_from_directory(
+    train_path,
+    target_size=(48,48),
+    color_mode="grayscale",
+    batch_size=batch_size,
+    class_mode="categorical"
+)
+
+test_data = datagen.flow_from_directory(
+    test_path,
+    target_size=(48,48),
+    color_mode="grayscale",
+    batch_size=batch_size,
+    class_mode="categorical"
+)
+
+model = Sequential()
+
+model.add(Conv2D(32, (3,3), activation='relu', input_shape=(48,48,1)))
+model.add(MaxPooling2D(2,2))
+
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(2,2))
+
+model.add(Conv2D(128, (3,3), activation='relu'))
+model.add(MaxPooling2D(2,2))
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(7, activation='softmax'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_data, epochs=25, validation_data=test_data)
+
+model.save("emotion_model.h5")
+print(" Model saved as emotion_model.h5")
